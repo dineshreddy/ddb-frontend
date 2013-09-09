bd2676df2c707fe23d31225de0042d5297e70eed
Christian Herlambang <christian.herlambang@fiz-karlsruhe.de>
2013-09-09 11:24:40 +0200

    DDBNEXT-765: wip

--- test/integration/de/ddb/next/SavedSearchServiceIntegrationTests.groovy
+++ test/integration/de/ddb/next/SavedSearchServiceIntegrationTests.groovy
@@ -21,4 +21,31 @@ class SavedSearchServiceIntegrationTests extends GroovyTestCase {
 
         log.info "The user ${userId} just saved search with the query string: ${queryString}, savedSearchId ${savedSearchId}"
     }
+
+    @Test
+    void shouldSavedUserSearchWithTitleAndDescription() {
+        log.info "should saved user search"
+
+        def userId = UUID.randomUUID() as String
+        def queryString = 'query=goethe&sort=ALPHA_ASC&facetValues[]=time_fct%3Dtime_62000&facetValues[]=time_fct%3Dtime_61600&facetValues[]=keywords_fct%3DFotos&facetValues[]=type_fct%3Dmediatype_002&facetValues[]=sector_fct%3Dsec_02'
+
+        def savedSearchId = savedSearchService.saveSearch(userId, queryString, 'Goethe Related', 'All things related to Goethe')
+        log.info "id: ${savedSearchId}"
+        assert savedSearchId  != null
+
+        log.info "The user ${userId} just saved search with the query string: ${queryString}, savedSearchId ${savedSearchId}"
+    }
+
+    @Test
+    void shouldRetrieveSavedSearch() {
+        log.info "should retrieve saved user search"
+
+        def userId = UUID.randomUUID() as String
+        def queryString = 'query=goethe&sort=ALPHA_ASC&facetValues[]=time_fct%3Dtime_62000&facetValues[]=time_fct%3Dtime_61600&facetValues[]=keywords_fct%3DFotos&facetValues[]=type_fct%3Dmediatype_002&facetValues[]=sector_fct%3Dsec_02'
+
+        def savedSearchId = savedSearchService.saveSearch(userId, queryString, 'Goethe Related', 'All things related to Goethe')
+        assert savedSearchId  != null
+
+        log.info "The user ${userId} just saved search with the query string: ${queryString}, savedSearchId ${savedSearchId}"
+    }
 }
