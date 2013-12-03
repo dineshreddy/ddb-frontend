1d234dfdb617773676a2b5a164bf62b9456c6dc9
hla <holger.lauinger@fiz-karlsruhe.de>
2013-12-03 14:53:22 +0100

    DDBNEXT-820: First compare version with iframes

---
+++ grails-app/controllers/de/ddb/next/CompareController.groovy
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2013 FIZ Karlsruhe
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.ddb.next
+
+class CompareController {
+
+    def configurationService
+
+    def index() {
+        log.info "index()"
+
+        def firstId = params.firstId
+        def secondId = params.secondId
+        log.info "index(): firstId="+firstId
+        log.info "index(): secondId="+secondId
+
+        def itemUri = request.forwardURI
+
+        render(view: "compare", model: [
+            firstId: firstId,
+            secondId: secondId,
+            itemUri: itemUri,
+            baseUrl: configurationService.getSelfBaseUrl()
+        ])
+    }
+}
