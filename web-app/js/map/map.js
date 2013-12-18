60611b79fd255f2b29c907858fd8bf3bf68acded
hla <holger.lauinger@fiz-karlsruhe.de>
2013-12-18 16:09:11 +0100

    HOTFIX: Deactivated server-side clustering for testmap

--- web-app/js/map/map.js
+++ web-app/js/map/map.js
@@ -85,30 +85,30 @@ $(document).ready(function() {
             //Show the waiting layer 
             self._showWaitingLayer();
             
-            self._loadClusteredInstitutionList(function() { //on clusters loaded
-              
-              //Draws the institutions on the vector layer
-              self._drawClustersOnMap();
-  
-              //Hide the waiting layer again
-              self._hideWaitingLayer();
-  
-              //Remove the tiles load listener again. We only want it on initialization.
-              tiles.events.unregister("loadend", tiles, onTilesLoaded);
-            });
-            
-//            //Loads all institutions over ajax
-//            self._loadFullInstitutionList(function() { //on build model finished
-//
+//            self._loadClusteredInstitutionList(function() { //on clusters loaded
+//              
 //              //Draws the institutions on the vector layer
 //              self._drawClustersOnMap();
-//
+//  
 //              //Hide the waiting layer again
 //              self._hideWaitingLayer();
-//
+//  
 //              //Remove the tiles load listener again. We only want it on initialization.
 //              tiles.events.unregister("loadend", tiles, onTilesLoaded);
 //            });
+            
+            //Loads all institutions over ajax
+            self._loadFullInstitutionList(function() { //on build model finished
+
+              //Draws the institutions on the vector layer
+              self._drawClustersOnMap();
+
+              //Hide the waiting layer again
+              self._hideWaitingLayer();
+
+              //Remove the tiles load listener again. We only want it on initialization.
+              tiles.events.unregister("loadend", tiles, onTilesLoaded);
+            });
           }
           tiles.events.register("loadend", tiles, onTilesLoaded);   
           
@@ -119,34 +119,34 @@ $(document).ready(function() {
       applyFilters : function() {
         var self = this;
         
-        //Show the waiting layer 
-        self._showWaitingLayer();
-          
-        self._loadClusteredInstitutionList(function(){
-          
-          //Draws the institutions on the vector layer
-          self._drawClustersOnMap();
-  
-          //Hide the waiting layer again
-          self._hideWaitingLayer();
-          
-        });
-        
-            
 //        //Show the waiting layer 
 //        self._showWaitingLayer();
-//
-//        window.setTimeout(function(){ //Give the browser time to display waiting gif
-//          self._buildModel(self.osmMap, self.institutionList, function() { //on build model finished
-//
-//            //Draws the institutions on the vector layer
-//            self._drawClustersOnMap();
-//
-//            //Hide the waiting layer again
-//            self._hideWaitingLayer();
-//
-//          });
-//        }, 100);
+//          
+//        self._loadClusteredInstitutionList(function(){
+//          
+//          //Draws the institutions on the vector layer
+//          self._drawClustersOnMap();
+//  
+//          //Hide the waiting layer again
+//          self._hideWaitingLayer();
+//          
+//        });
+        
+            
+        //Show the waiting layer 
+        self._showWaitingLayer();
+
+        window.setTimeout(function(){ //Give the browser time to display waiting gif
+          self._buildModel(self.osmMap, self.institutionList, function() { //on build model finished
+
+            //Draws the institutions on the vector layer
+            self._drawClustersOnMap();
+
+            //Hide the waiting layer again
+            self._hideWaitingLayer();
+
+          });
+        }, 100);
       },
       
       
