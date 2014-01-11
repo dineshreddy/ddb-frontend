d67ed25a884a22444bdf48b0b2f592fa731fb6eb
A Brahaj <armand.brahaj@fiz-karlsruhe.de>
2014-01-11 01:54:24 +0100

    Fix Copyright Date

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -1,7 +1,7 @@
 import de.ddb.next.constants.FacetEnum
 
 /*
- * Copyright (C) 2013 FIZ Karlsruhe
+ * Copyright (C) 2014 FIZ Karlsruhe
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
