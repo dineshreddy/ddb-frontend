96ac72fbaae7bf10cf88ebed499306bf842fc5c5
aschenk <andre.schenk@fiz-karlsruhe.de>
2014-01-23 15:04:19 +0100

    https://jira.deutsche-digitale-bibliothek.de/browse/DDBNEXT-1131: It't not possible to copy entity pages to individual favorites lists

--- grails-app/controllers/de/ddb/next/FavoritesController.groovy
+++ grails-app/controllers/de/ddb/next/FavoritesController.groovy
@@ -1,5 +1,19 @@
+/*
+ * Copyright (C) 2014 FIZ Karlsruhe
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package de.ddb.next
-
 
 import grails.converters.JSON
 
@@ -13,9 +27,8 @@ import de.ddb.next.constants.SearchParamEnum
 import de.ddb.next.constants.Type
 import de.ddb.next.exception.FavoritelistNotFoundException
 
-
 class FavoritesController {
-
+    def bookmarksService
 	def favoritesService
 
 	private def reportFavoritesList(String userId, String folderId){
@@ -408,7 +421,7 @@ class FavoritesController {
 									user.getId(),
 									itemId,
 									favoriteToCopy.creationDate.getTime(),
-									Type.CULTURAL_ITEM,
+									favoriteToCopy.type,
 									[folderId],
 									"",
 									new Date().getTime())
@@ -419,23 +432,14 @@ class FavoritesController {
 			}else{
 				result = response.SC_UNAUTHORIZED
 			}
-
-
-			result = response.SC_OK
-
-			flash.message = "ddbnext.favorites_copy_succ"
-
+            result = response.SC_OK
+            flash.message = "ddbnext.favorites_copy_succ"
 		} else {
 			result = response.SC_UNAUTHORIZED
 		}
-
 		log.info "copyFavorites returns " + result
 		render(status: result)
-
-
 	}
-
-
 
 	private def unblockFavoritesList(String userId, String folderId, String unblockingToken){
 		log.info "unblockFavoritesList()"
