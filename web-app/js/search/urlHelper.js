a040fc6613dfcfb8656aa897066697559b4341f1
ARB <armand.brahaj@fiz-karlsruhe.de>
2014-05-13 16:26:15 +0200

    Intendation

--- web-app/js/search/urlHelper.js
+++ web-app/js/search/urlHelper.js
@@ -120,8 +120,8 @@ de.ddb.next.search.getUrlVars = function() {
 };
 
 de.ddb.next.search.getParameterByName = function (name) {
-    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
-      var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
-        results = regex.exec(location.search);
-        return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+  name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+  var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+  results = regex.exec(location.search);
+  return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
 };
