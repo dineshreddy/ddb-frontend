3674cad845f095d6123ed31db285c5f075eb10df
ARB <armand.brahaj@fiz-karlsruhe.de>
2014-06-17 14:05:30 +0200

    Fix DDBNEXT-1590 Design issues for person overview page

--- web-app/js/persons.js
+++ web-app/js/persons.js
@@ -26,6 +26,7 @@ $(function() {
     imagesLoaded( container, function() {
       new Masonry( container, {
         // options
+        isOriginLeft: false,
         columnWidth: 170,
         itemSelector: '.pin'
       });
