593d86ad803989b4d0e29bca45d5309d56e4f495
ARB <armand.brahaj@fiz-karlsruhe.de>
2014-09-23 15:18:21 +0200

    Remove Public Url / Add Logging

--- grails-app/services/de/ddb/next/ConfigurationService.groovy
+++ grails-app/services/de/ddb/next/ConfigurationService.groovy
@@ -82,10 +82,6 @@ class ConfigurationService extends CommonConfigurationService {
         return getConfigValue("ddb.culturegraph.url")
     }
     
-    public String getDomainCanonic(){
-        return getConfigValue("ddb.domain.canonic")
-    }
-
     public String getGrailsMailHost(){
         return getConfigValue("grails.mail.host")
     }
