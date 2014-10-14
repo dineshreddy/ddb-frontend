ffbf3546ead0631143bfc56ee0949adac5b82859
boz <stefan.bozic@fiz-karlsruhe.de>
2014-10-14 13:29:37 +0200

    Bumped ddb-common version number to 0.13-SNAPSHOT

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -113,7 +113,7 @@ grails.project.dependency.resolution = {
 
         if ((environment != Environment.DEVELOPMENT)|| (!localDdbCommonFound))  {
             println "Using maven repo for common plugin"
-            compile "de.ddb:ddb-common:0.12-SNAPSHOT"
+            compile "de.ddb:ddb-common:0.13-SNAPSHOT"
         }
     }
 
