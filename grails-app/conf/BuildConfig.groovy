15dbc02587d63a9e48bf73be0d8134197501a507
aschenk <andre.schenk@fiz-karlsruhe.de>
2015-04-08 11:53:57 +0200

    added dependency to scribe

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -88,6 +88,7 @@ grails.project.dependency.resolution = {
     dependencies {
         runtime 'org.ccil.cowan.tagsoup:tagsoup:1.2.1'
         runtime 'org.openid4java:openid4java:0.9.8'
+        runtime 'org.scribe:scribe:1.3.0-patched'
         runtime 'org.springframework:spring-test:3.2.8.RELEASE'
         runtime ('org.codehaus.groovy.modules.http-builder:http-builder:0.5.2') { excludes "groovy" }
 
