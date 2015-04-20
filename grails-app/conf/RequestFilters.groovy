1122121bfe38b682018d116ed7ef3a5fac635eaa
aschenk <Andre.Schenk@fiz-Karlsruhe.de>
2015-04-20 17:02:20 +0200

    reverted last change

--- grails-app/conf/RequestFilters.groovy
+++ grails-app/conf/RequestFilters.groovy
@@ -42,9 +42,6 @@ class RequestFilters {
                         currentLocale = configurationService.getDefaultLanguage()
                         localeResolver.setLocale(request, response,  currentLocale)
                         session.locale = currentLocale
-
-                        // set default locale for DdbLocaleResolver
-                        Locale.setDefault(currentLocale)
                     }
                 }
             }
