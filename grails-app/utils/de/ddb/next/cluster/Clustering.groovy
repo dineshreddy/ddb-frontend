9f7ed6a974c657efc2ab8032ccebf5bb8292ad51
aschenk <andre.schenk@fiz-karlsruhe.de>
2015-05-06 12:49:37 +0200

    fixed Sonar issue

--- grails-app/utils/de/ddb/next/cluster/Clustering.groovy
+++ grails-app/utils/de/ddb/next/cluster/Clustering.groovy
@@ -435,10 +435,10 @@ class Clustering {
         //console.info("Test 2: Edges Facets (null) ...");
         for (def i=0; i<this.edges.size();i++ ) {
             def e = this.edges[i]
-            if (e.leftFace == null || e.rightFace == null) {
-                //console.info(e);
-                //alert();
-            }
+            // if (e.leftFace == null || e.rightFace == null) {
+            //     console.info(e)
+            //     alert()
+            // }
         }
 
         def c = 0
