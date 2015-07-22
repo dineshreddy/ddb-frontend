8a8fb1a7a8e47995bd339bb54acbbbdb5373c45d
aschenk <andre.schenk@fiz-karlsruhe.de>
2015-07-22 15:33:24 +0200

    https://jira.deutsche-digitale-bibliothek.de/browse/DDBNEXT-2141: Update target URL on login and registration page

--- grails-app/controllers/de/ddb/next/UserController.groovy
+++ grails-app/controllers/de/ddb/next/UserController.groovy
@@ -73,7 +73,7 @@ class UserController {
         render(view: "login",
         model: ['loginStatus': LoginStatus.LOGGED_OUT,
             'referrer': params.referrer,
-            'registrationInfoUrl': configurationService.getContextUrl() + configurationService.getRegistrationInfoUrl()
+            'registrationInfoUrl': configurationService.getRegistrationInfoUrl()
         ])
     }
 
@@ -255,7 +255,7 @@ class UserController {
 
     private def getRegistrationUrls() {
         return [
-            registrationInfoUrl: configurationService.getContextUrl() + configurationService.getRegistrationInfoUrl(),
+            registrationInfoUrl: configurationService.getRegistrationInfoUrl(),
             accountTermsUrl: configurationService.getContextUrl() + configurationService.getAccountTermsUrl(),
             accountPrivacyUrl: configurationService.getContextUrl() + configurationService.getAccountPrivacyUrl()
         ]
