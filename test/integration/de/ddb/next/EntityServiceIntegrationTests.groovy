48760cae8ff9cdd356f858f2ac483fb092c65154
aschenk <Andre.Schenk@fiz-Karlsruhe.de>
2016-01-15 16:40:27 +0100

    changed mixin

--- test/integration/de/ddb/next/EntityServiceIntegrationTests.groovy
+++ test/integration/de/ddb/next/EntityServiceIntegrationTests.groovy
@@ -2,24 +2,21 @@ package de.ddb.next
 
 import static org.junit.Assert.*
 import grails.test.mixin.TestMixin
-import grails.test.mixin.integration.IntegrationTestMixin
+import grails.test.mixin.web.ControllerUnitTestMixin
 
 import org.junit.*
 
-import de.ddb.common.constants.RoleFacetEnum
-
-
-@TestMixin(IntegrationTestMixin)
+@TestMixin(ControllerUnitTestMixin)
 class EntityServiceIntegrationTests {
 
     def entityService
-    
+
     @Test void doEntitySearch() {
         def result = entityService.doEntitySearch(["query":"Walther Klemm"])
         log.info result
         assertNotNull result
     }
-    
+
     @Test void getEntityDetails() {
         assertNotNull entityService.getEntityDetails("118563165")
     }
